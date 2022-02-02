5c5fa410a68cfdb13b3f8be91f42fac98fe0cdc8
RubyCircle <pavel.jakushik@yandex.by>
2022-02-02 15:54:55 +0300

    Added class Event * The event class implements the basic logic for working with events. * Remove GameData.cpp

---
+++ LimeEngine/Engine/Base/Events.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <list>
+#include <functional>
+
+using EventId = size_t;
+
+template<typename... TArgs>
+struct Event
+{
+	Event(EventId id, std::function<void(TArgs...)> func = nullptr) noexcept : id(id), func(func) {}
+	bool operator==(const Event& other) const noexcept
+	{
+		return id == other.id;
+	}
+	bool operator==(const EventId otherId) const noexcept
+	{
+		return id == otherId;
+	}
+	bool operator!=(const Event& other) const noexcept
+	{
+		return !(*this == other);
+	}
+	bool operator!=(const EventId& otherId) const noexcept
+	{
+		return !(*this == otherId);
+	}
+	void operator()(TArgs... args)
+	{
+		func(std::forward<TArgs>(args)...);
+	}
+	EventId id;
+	std::function<void(TArgs...)> func;
+};
+
+template<typename... TArgs>
+class EventDispatcher
+{
+public:
+	EventDispatcher() = default;
+	EventId Bind(std::function<void(TArgs...)>&& f)
+	{
+		events.push_back({ next_id,  std::move(f) });
+		return next_id++;
+	}
+	void Unbind(EventId id)
+	{
+		events.remove(id);
+	}
+	EventId operator+=(std::function<void(TArgs...)>&& f)
+	{
+		Bind(std::move(f));
+	}
+	void operator-=(EventId id)
+	{
+		Unbind(id);
+	}
+	void operator()(TArgs... args)
+	{
+		for (auto&& f : events)
+		{
+			f(std::forward<TArgs>(args)...);
+		}
+	}
+private:
+	size_t next_id = 0;
+	std::list<Event<TArgs...>> events;
+};
